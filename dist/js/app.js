c3c5bb31fb4b80b4b1cca8149f470ddbf469e1bb
Kateryna <zbsht15@gmail.com>
2015-04-17 10:29:01 -0700

    Updated/fixed  html/js

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,33 +1,37 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var app = angular.module("ASDFASDF", []);
+var application = angular.module("ASDFASDF", []);
 
-// function TodoCtrl($scope) {
-//   $scope.todos = [
-//     {text:'learn angular', done:true},
-//     {text:'build an angular app', done:false}];
- 
-//  $scope.arch = [];
- 
-//   $scope.addTodo = function() {
-//     $scope.todos.push({text:$scope.todoText, done:false});
-//     $scope.todoText = '';
-//   };
- 
-//   $scope.remaining = function() {
-//     var count = 0;
-//     angular.forEach($scope.todos, function(todo) {
-//       count += todo.done ? 0 : 1;
-//     });
-//     return count;
-//   };
- 
-//   $scope.archive = function() {
-//     var oldTodos = $scope.todos;
-//     $scope.todos = [];
-//     angular.forEach(oldTodos, function(todo) {
-//       if (!todo.done) $scope.todos.push(todo);
-// 	  else $scope.arch.push(todo)
-//     });
-//   };
-// }
+application.controller("TodoCtrl", ["$scope", function($scope){
+	$scope.todos = [
+		{text:'learn angular', done:true},
+		{text:'build an angular app', done:false},
+				{text:'build an angular app 2', done:true}
+
+	];
+
+	$scope.arch = [];
+
+	$scope.addTodo = function() {
+		$scope.todos.push({text:$scope.todoText, done:false});
+		$scope.todoText = '';
+	};
+
+	$scope.remaining = function() {
+		var count = 0;
+		angular.forEach($scope.todos, function(todo) {
+			count += todo.done ? 0 : 1;
+		});
+		return count;
+	};
+
+	$scope.archive = function() {
+		var oldTodos = $scope.todos;
+		$scope.todos = [];
+		angular.forEach(oldTodos, function(todo) {
+			if (!todo.done) $scope.todos.push(todo);
+			else $scope.arch.push(todo)
+		});
+	};
+}]);
+
 },{}]},{},[1]);
